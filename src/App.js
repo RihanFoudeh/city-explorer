54c8cac605743a69c856f22d36df015451a19b5f
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-09-05 16:30:35 +0300

    adding task2

--- src/App.js
+++ src/App.js
@@ -1,2 +1,101 @@
 import axios from "axios";
-import react from "react";
+import React from "react";
+
+import { Form, Button , Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+
+
+constructor (props){
+  super(props);
+
+  this.state={
+
+    lat:'',
+    lon:'',
+    displayName:'',
+    mapFlag:false,
+    displayErr:false,
+
+
+
+  }
+}
+
+getdata = async (event)  =>{
+
+  event.preventDefault();
+
+
+  const  location =event.target.location.value;
+
+// console.log(location);
+
+
+const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${location}&format=json`
+
+
+
+
+let getsourse = await axios.get(url);
+
+
+
+this.setState({
+
+lat:getsourse.data[0].lat ,
+lon:getsourse.data[0].lon,
+displayName:getsourse.data[0].display_name
+
+
+// 
+
+
+
+
+})
+console.log(this.state.lat);
+
+}
+
+
+
+
+
+
+  render() {
+
+    return (
+
+
+
+      <>
+
+        <Form onSubmit={this.getdata}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Enter placeName</Form.Label>
+            <Form.Control name="location" type="text" placeholder="Enter placeName" />
+
+          </Form.Group>
+          <Button type="submit">
+            Explore!
+          </Button>
+        </Form>
+
+      <p>Display name : {this.state.displayName}</p>
+      <p>Lat : {this.state.lat}</p>
+      <p>Lon : {this.state.lon}</p>
+
+
+
+      </>
+
+
+
+    );
+  }
+}
+
+
+export default App;
