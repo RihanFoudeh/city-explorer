7accf9883500612e1a89fc5d055fa3f310fd8356
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-09-05 18:18:03 +0300

    adding task4

--- src/App.js
+++ src/App.js
@@ -1,104 +1,106 @@
 import axios from "axios";
 import React from "react";
 import './App.css';
-import { Form, Button , Image } from 'react-bootstrap';
+import { Form, Button, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
 
-constructor (props){
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state={
+    this.state = {
 
-    lat:'',
-    lon:'',
-    displayName:'',
-    mapFlag:false,
-    displayErr:false,
+      lat: '',
+      lon: '',
+      displayName: '',
+      mapFlag: false,
+      displayErr: false,
+newtext:false,
 
 
+    }
+  }
+
+  getdata = async (event) => {
+
+    event.preventDefault();
+
+
+    const location = event.target.location.value;
+
+    // console.log(location);
+
+
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${location}&format=json`
+
+
+
+    try {
+      let getsourse = await axios.get(url);
+
+
+
+      this.setState({
+
+        lat: getsourse.data[0].lat,
+        lon: getsourse.data[0].lon,
+        displayName: getsourse.data[0].display_name,
+        newtext:true
+      })
+      // console.log(this.state.lat);
+    }
+
+    catch {
+      this.setState({
+        displayErr: true,
+        newtext: false,
+      })
+    }
 
   }
-}
-
-getdata = async (event)  =>{
-
-  event.preventDefault();
-
-
-  const  location =event.target.location.value;
-
-// console.log(location);
-
-
-const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${location}&format=json`
 
 
 
 
-let getsourse = await axios.get(url);
+render() {
 
-
-
-this.setState({
-
-lat:getsourse.data[0].lat ,
-lon:getsourse.data[0].lon,
-displayName:getsourse.data[0].display_name
-
-
-// 
-
-
-
-
-})
-console.log(this.state.lat);
-
-}
-
-
-
-
-
-
-  render() {
-
-    return (
-
-<>
+  return (
+    <>
       <body className="body">
-      <header className="Head">City Explorer </header>
+        <header className="Head">City Explorer </header>
 
         <Form className="Form" onSubmit={this.getdata}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Enter placeName</Form.Label>
-            <Form.Control id="control"  name="location" type="text" placeholder="Enter placeName" />
+            <Form.Control  name="location" type="text" placeholder="Enter placeName" />
 
           </Form.Group>
-          <Button  type="submit">
+          <Button type="submit">
             Explore!
           </Button>
         </Form>
-<h1>Welcome To {this.state.displayName} </h1>
-      <p> {this.state.displayName} is located at {this.state.lat} by  {this.state.lon}</p>
-      {/* <p>Lat : </p>
-      <p>Lon :</p> */}
 
-      <Image className="image" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.lon}&zoom=[1 to 18]`}  fluid />
+        {this.state.newtext && <h1>Welcome To {this.state.displayName} </h1>}
 
+      {this.state.newtext &&  <p> {this.state.displayName} is located at {this.state.lat} by  {this.state.lon}</p>}
 
+        {this.state.displayErr && <p>Error: Status Code: 400, 404, 500</p>}
 
-      <footer className='footer' >&copy; Rihan Foudeh</footer>      </body>
+        {this.state.newtext &&  <Image className="image" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.lon}&zoom=[1 to 18]`} fluid />}
 
 
-      </>
 
-    );
-  }
+        <footer className='footer' >&copy; Rihan Foudeh</footer>      </body>
+
+
+    </>
+
+  );
 }
+}
+
 
 
 export default App;
