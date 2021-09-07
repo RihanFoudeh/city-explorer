7d8ce561e5d0e037a32ac92d94fb28ba03126c2f
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-09-07 14:36:27 +0300

    finish lab07

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,19 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Weather extends React.Component{
+  
     render(){
         
         return(
             <>
             <br></br>
-                {!(this.props.weatherErr) && this.props.day &&  <p>date: {this.props.day}</p>}
-                {!(this.props.weatherErr)&&this.props.description && <p>description: {this.props.description}</p>}
-                <br></br>
-                {!(this.props.weatherErr) && this.props.day2 &&<p>date: {this.props.day2}</p>}
-                {!(this.props.weatherErr)&&this.props.description2 && <p>description: {this.props.description2}</p>}
-                <br></br>
-                {!(this.props.weatherErr) &&this.props.day3 && <p>date: {this.props.day3}</p>}
-                {!(this.props.weatherErr)&&this.props.description3 && <p>description: {this.props.description3}</p>}
+                
+                {this.props.weatherData.map(item=>{
+                    return ( 
+                    <>
+                    <p>date: {item.date}</p>
+                    <p>description: {item.description}</p>
+                    </>
+                    )
+                })}
+
                 {this.props.weatherErr && <p>Status Code: 400, 404, 500</p>}
             </>
         )
